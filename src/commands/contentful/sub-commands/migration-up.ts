c2bfa45ca2b9e4a2ba9a8d2efec8164e94662b64
Parker Hutchinson <hutchinsonparker@gmail.com>
2021-10-25 10:03:02 -0700

    put in a safegaurd so that we dont run migrations on master aliases

--- src/commands/contentful/sub-commands/migration-up.ts
+++ src/commands/contentful/sub-commands/migration-up.ts
@@ -2,6 +2,7 @@ import run from "../../../utils/run.ts";
 import log from "../../../utils/log.ts";
 import { Command, Flags } from "../../../types/command.d.ts";
 import { readEnvFile } from "../../../lib/env-io.ts";
+import { manageContentfulData } from "../../../lib/contentful-data.ts";
 
 /**
  * @description A subprocess call to contentful-migrate to run the "up"
@@ -16,35 +17,48 @@ const migrationUp = async (args: Flags) => {
   const manageToken: string | undefined = Deno.env.get(
     "CONTENTFUL_MANAGEMENT_ACCESS_TOKEN"
   );
+  const { status: _environmentStatus, data: environmentData } =
+    await manageContentfulData(`environments`, "GET");
+
   const envId: string | undefined = await readEnvFile();
+  // get the current master alias env to make sure we dont run migrations on master.
+  // @ts-ignore: why
+  const masterEnvIds = environmentData.items
+    // @ts-ignore: 
+    .map((item) => ({ name: item.name, id: item.sys.id }))
+    // @ts-ignore: 
+    .filter((item) => item.id === 'master')[0];
 
   if (dryRun) {
     log("executing dry on pending migrations", "cyan");
   }
-
-  console.log(`\n running pending migrations on ${envId} environment \n`);
-
-  if (dryRun) {
-    await run([
-      "npx",
-      "contentful-migrate",
-      "up",
-      `--access-token=${manageToken}`,
-      `--space-id=${spaceId}`,
-      `--environment-id=${envId}`,
-      "--all",
-      "-d",
-    ]);
+  
+  if (masterEnvIds.name !== envId) {
+    console.log(`\n running pending migrations on ${envId} environment \n`);
+    if (dryRun) {
+      await run([
+        "npx",
+        "contentful-migrate",
+        "up",
+        `--access-token=${manageToken}`,
+        `--space-id=${spaceId}`,
+        `--environment-id=${envId}`,
+        "-d",
+        "--all",
+      ]);
+    } else {
+      await run([
+        "npx",
+        "contentful-migrate",
+        "up",
+        `--access-token=${manageToken}`,
+        `--space-id=${spaceId}`,
+        `--environment-id=${envId}`,
+        "--all",
+      ]);
+    }
   } else {
-    await run([
-      "npx",
-      "contentful-migrate",
-      "up",
-      `--access-token=${manageToken}`,
-      `--space-id=${spaceId}`,
-      `--environment-id=${envId}`,
-      "--all",
-    ]);
+    log(`can't run migration on environment ${envId} as its currently the alias for master \n`, "red");
   }
 };
 
